17f08aa20f19b31a1eaa8ad63666d32bf9200420
MikeTheShadow <michaelpape11@gmail.com>
2023-07-11 16:59:04 -0600

    Updated some minor things

--- dataset_checker.py
+++ dataset_checker.py
@@ -1,3 +1,3 @@
 from datasets import load_dataset
 
-my_dataset = load_dataset('json', data_dir='scripts/train/custom_datasets', split='train')
+my_dataset = load_dataset('json', data_dir='scripts/train/custom_datasets/data', split='train')
